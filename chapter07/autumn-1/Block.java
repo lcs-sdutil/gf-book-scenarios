6022afe301b65b07df8d59c5b3ca73cf2e3865f7
Simon Dutil <sdutil@lcs.on.ca>
2019-04-26 13:57:49 -0400

    remove leaf when touches block

--- chapter07/autumn-1/Block.java
+++ chapter07/autumn-1/Block.java
@@ -9,7 +9,7 @@ import greenfoot.*;
 public class Block extends Actor
 {
     private int delta = 2;
-    
+
     /**
      * Move across the screen, bounce off edges. Turn leaves, if we touch any.
      */
@@ -18,9 +18,31 @@ public class Block extends Actor
         move();
         checkEdge();
         checkMouseClick();
-        
+        checkForLeafCollision(); 
     }
-    
+
+    /**
+     * remove a Leaf object if we are intersecting with it 
+     */
+    private void checkForLeafCollision() 
+    {
+        // Get an object reference to a single Leaf from any object we are currently
+        // intersecting
+        // NOTE : by placing a type in brackets before the call to getOneIntersectingObject
+        // we ***cast*** ( or force ) the type to cgange from Actor to leaf
+        // NOTE 2: We can do this because Leaf is a subclass of actor.
+        Leaf someLeaf = (Leaf) getOneIntersectingObject(Leaf.class);
+
+        // Check that the someLeaf object actually has areference to an object
+        // If it is null, that mean there was ne Leaf interseecting the block this time around 
+        if (someLeaf != null)
+        {
+            // someLeaf is not null... so now we can remove it 
+            World myWorld = getWorld();
+            myWorld.removeObject(someLeaf);
+        }
+    }
+
     /**
      * Move sideways, either left or right.
      */
@@ -28,7 +50,7 @@ public class Block extends Actor
     {
         setLocation(getX()+delta, getY());
     }
-    
+
     /**
      * Check whether we are at the edge of the screen. If we are, turn around.
      */
@@ -37,15 +59,15 @@ public class Block extends Actor
         if (isAtEdge()) 
         {
             delta = -delta;  // reverse direction
-            
+
             // add a new Leaf object to the world 
             //1. Add a reference to the world
             World myWorld =getWorld();
             //2. Use the object reference to call, or use, a method on the World object
-            myWorld.addObject(new Leaf(), getX(),getY());
+            myWorld.addObject(new Leaf(), getX() ,getY() -50);
         }
     }
-    
+
     /**
      * Check whether the mouse button was clicked. If it was, change all leaves.
      */
@@ -56,5 +78,5 @@ public class Block extends Actor
             // do this when the mouse is clicked. currently: nothing.
         }
     }
-    
+
 }
